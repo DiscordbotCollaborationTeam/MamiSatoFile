49149437d59b05d320d53d45e196b1b1e0aeb574
jun50 <0001jun50@gmail.com>
2019-01-20 11:36:18 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,33 @@
+import discord
+from discord.ext import commands
+import json
+
+bot = commands.Bot(command_prefix='!sm:')
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print ('-------')
+
+@bot.event
+async def on_message_edit(old, new):
+    msg=new
+    if not msg.author.bot:
+        return
+    if msg.channel.id not in [channel_id]:
+        return
+    if not new.embeds:
+        return
+    embed=new.embeds[0]
+    user=str(embed.title)
+    AofM=int(embed.description)
+    if AofM < 0:
+        return
+    with open("currency.json") as f:
+        df = json.load(f)
+    df.setdefault(user, 0)
+    df[user] += AofM
+
+bot.run(Token)
